81999081f2ac3f77c9a714c101fc64bb3d7d7122
Víctor Quirino <victorsouzaq@gmail.com>
2023-10-20 00:28:01 -0300

    Parte individual da instrução prática 19

---
+++ ItemSet.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class ItemSet{
+    private:
+        vector<string> itens;
+    public:
+        void inserirItem(string);
+        void excluirItem(string);
+};
+
+void ItemSet::inserirItem(string s){
+    bool jaExiste = false;
+
+    for(string item : itens) if(item == s) jaExiste = true;
+
+    if(jaExiste){
+        cout<<s<<" ja existe no conjunto de itens!"<<endl;
+        return;
+    } 
+
+   itens.push_back(s);
+   cout<<"Item "<<s<<" adicionado com sucesso!"<<endl;
+}
+
+void ItemSet::excluirItem(string s){
+    for(int i = 0; i < itens.size(); i++){
+        if(itens.at(i) == s){
+            itens.erase(itens.begin() + i);
+            cout<<s<<" foi removido com sucesso!"<<endl;
+            return;
+        } 
+    }
+
+    cout<<s<<" nao existe no conjunto de dados!"<<endl;
+}
+
+int main(){
+    ItemSet set;
+
+    set.inserirItem("abc");
+    set.inserirItem("bcd");
+    set.inserirItem("dce");
+
+    //tentando adicionar item repetido
+    set.inserirItem("abc");
+
+    //Removendo item que existe no conjunto
+    set.excluirItem("bcd");
+
+    //Tentando remover item que não existe no conjunto
+    set.excluirItem("bdc");
+
+}
